98d6de5d9a978c08807ae940e3ce5462f5d2124a
Ji-Haeng Huh <jhhuh.note@gmail.com>
2025-02-05 23:42:15 +0900

    add devshell with nixos-shell

---
+++ nixos.kr/flake.nix
@@ -0,0 +1,23 @@
+{
+
+  inputs.nixpkgs.url = github:nixos/nixpkgs/nixos-24.11;
+
+  outputs = inputs:
+
+  let
+
+    system = "x86_64-linux";
+
+    pkgs = import inputs.nixpkgs { inherit system; };
+
+    dev-shell = pkgs.mkShell { buildInputs = [ pkgs.nixos-shell ]; };
+
+  in
+
+  {
+
+    devShells.${system}.default = dev-shell;
+    
+  };
+
+}
