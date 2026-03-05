faa3c294b05dc792748caca1fb987d2664f9ae4b
Ji-Haeng Huh <jhhuh.note@gmail.com>
2026-03-06 02:42:16 +0900

    rewrite flake.nix for Emanote via flake-parts
    Replace Hakyll-based setup with Emanote static site generator.
    Uses ko/ and global/ content layers, cache.nixos.asia binary cache.

--- nixos.kr/flake.nix
+++ nixos.kr/flake.nix
@@ -1,33 +1,32 @@
 {
-
-  inputs.nixpkgs.url = github:nixos/nixpkgs/nixos-24.11;
-
-  outputs = inputs:
-
-  let
-
-    inherit (inputs.nixpkgs) lib;
-
-    system = "x86_64-linux";
-
-    pkgs = import inputs.nixpkgs { inherit system; };
-
-    dev-shell = pkgs.mkShell { buildInputs = [ pkgs.nixos-shell ]; };
-
-  in
-
-  {
-
-    devShells.${system}.default = dev-shell;
-
-    nixosConfigurations.vm = lib.nixosSystem {
-      inherit system;
-      modules = [
-        ./port-forward.nix
-        ./static-web-server.nix
-      ];
-    };
-
+  nixConfig = {
+    extra-substituters = "https://cache.nixos.asia/oss";
+    extra-trusted-public-keys = "oss:KO872wNJkCDgmGN3xy9dT89WAhvv13EiKncTtHDItVU=";
   };
 
+  inputs = {
+    emanote.url = "github:srid/emanote";
+    emanote.inputs.emanote-template.follows = "";
+    nixpkgs.follows = "emanote/nixpkgs";
+    flake-parts.follows = "emanote/flake-parts";
+  };
+
+  outputs = inputs@{ self, flake-parts, nixpkgs, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [ "x86_64-linux" "aarch64-linux" "aarch64-darwin" ];
+      imports = [ inputs.emanote.flakeModule ];
+      perSystem = { self', pkgs, ... }: {
+        emanote.sites = {
+          default = {
+            layers = [
+              { path = ./global; pathString = "./global"; }
+              { path = ./ko; pathString = "./ko"; }
+            ];
+          };
+        };
+        devShells.default = pkgs.mkShell {
+          buildInputs = [ pkgs.nixpkgs-fmt ];
+        };
+      };
+    };
 }
